0265545d1afaa90a5449160dd55982527c09d9b3
hyl2048 <2745506937@qq.com>
2024-05-25 23:02:43 +0800

    add download

--- download.py
+++ download.py
@@ -1,7 +1,12 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(
-    repo_id="rrsong/utc-base",
-    ignore_patterns=["*.h5", "*.ot", "*.msgpack"],
-    local_dir="/root/autodl-tmp/models/utc_base_pytorch",
-)
+# snapshot_download(
+#     repo_id="rrsong/utc-base",
+#     ignore_patterns=["*.h5", "*.ot", "*.msgpack"],
+#     local_dir="/root/autodl-tmp/models/utc_base_pytorch",
+# )
+from paddlenlp.transformers import UTC, ErnieTokenizer
+
+tokenier = ErnieTokenizer.from_pretrained("utc-base")
+model = UTC.from_pretrained("utc-base")
+print(tokenier, model)
